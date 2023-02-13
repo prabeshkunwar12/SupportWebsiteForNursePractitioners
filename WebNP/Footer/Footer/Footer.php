c8e91b182c2f9bad6671753cf01d5a84059dcc83
YoussefMT <97671854+YoussefMT@users.noreply.github.com>
2023-02-13 09:01:31 -0500

    Add files via upload

---
+++ WebNP/Footer/Footer/Footer.php
@@ -0,0 +1,77 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-info">
+                        <h3>WebNP</h3>
+                        <p>
+                            <strong>Phone:</strong> +1 234 5678 910<br>
+                            <strong>Email:</strong> webnp@example.com<br>
+                        </p>
+                        <div class="social-links mt-3">
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#about">Topics</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#services">About us</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#doctor">Contact</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#contact">Feedback</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Link</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Link</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Link</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Link</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-4 col-md-6 search-container">
+                    <h4>Find the topic you're looking for!</h4>
+                    <form action="/action_page.php">
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>WebNP</span></strong>. All Rights Reserved
+        </div>
+        <div class="disclaimer">
+            Disclaimer
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/vendor/php-email-form/validate.js"></script>
+<script src="/vendor/purecounter/purecounter.js"></script>
+<script src="/vendor/swiper/swiper-bundle.min.js"></script>
+
+<!-- Template Main JS File -->
+<script src="/js/main.js"></script>
---
+++ WebNP/Footer/Footer/header.php
@@ -0,0 +1,12 @@
+ <!-- Vendor CSS Files -->
+    <link href="/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="/css/style.css" rel="stylesheet">
